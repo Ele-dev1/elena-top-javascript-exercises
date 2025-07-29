0f1c0ae2cec3bb211d02158bc08e530397d3513c
Elena <209527620+Ele-dev1@users.noreply.github.com.>
2025-07-29 11:02:10 +0200

    complete exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
 
+    if (num1 < num2 && num1 > 0 && num2 > 0 && num1 % 1 === 0 && num2 % 1 === 0 && typeof(num1) === 'number' && typeof(num2) === 'number') {
+        for (let i = num1; i <= num2; i++) {
+            sum += i;
+        } 
+    } else if (num1 > num2 && num1 > 0 && num2 > 0 && num1 % 1 === 0 && num2 % 1 === 0 && typeof(num1) === 'number' && typeof(num2) === 'number') {
+      for (let i = num1; i >= num2; i--) {
+            sum += i;
+        }  
+    } else {
+        sum = "ERROR";
+    }
+
+    return sum
 };
 
 // Do not edit below this line
