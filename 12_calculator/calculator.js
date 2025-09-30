05280148f1d7096228bd704189b5ed1fa95e6a60
Elena <209527620+Ele-dev1@users.noreply.github.com.>
2025-09-30 16:48:06 +0200

    complete exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let sum = 0;
+	for (let num of numbers) {
+    sum += num;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let result = numbers.reduce((product, factor) => product * factor);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+  return number **power;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let result = 1;
+  for (let i = 1; i <= number; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
