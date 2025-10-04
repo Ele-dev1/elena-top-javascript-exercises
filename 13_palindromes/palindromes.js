5dde203f0f0e8521d62105c789182a749fc4fe5c
Elena <209527620+Ele-dev1@users.noreply.github.com.>
2025-10-04 15:49:11 +0200

    complete exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let splitted = string.split("");
+    //work with puntuaction
+    let puntAndSpace = ['.', ',', '!', '?', ':', ';', '-', ' '];
+    let cleaned = [];
+    for (let char of splitted) {
+        if (!(puntAndSpace.includes(char))) {
+            cleaned.push(char);
+        }
+    }
+    let joined =  cleaned.join('').toLowerCase();
+    let reversedJoined =  cleaned.reverse().join('').toLowerCase();
+    if (joined === reversedJoined) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
