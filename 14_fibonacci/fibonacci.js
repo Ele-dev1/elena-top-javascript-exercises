730766b769ae0960a7259396d0e447f1b024b9fd
Elena <209527620+Ele-dev1@users.noreply.github.com.>
2025-11-16 13:06:18 +0100

    complete exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let firstNumber = 0;
+    let secondNumber = 1;
+    if (number == 0) {
+        return 0;
+    } else if (number < 0) {
+        return "OOPS";
+    }
+    for (let i = 1; i < number; i++) {
+        let sum = firstNumber + secondNumber;
+        firstNumber = secondNumber;
+        secondNumber = sum; 
+    }
+    return secondNumber;
 };
 
 // Do not edit below this line
